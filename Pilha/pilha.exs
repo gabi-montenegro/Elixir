1b4aa28f4da919e1d185cd8aa2e444ecd0460be2
gabriella <gabriellamontenegro2@gmail.com>
2022-04-08 08:50:43 -0300

    pilha atualizada

--- Pilha/pilha.exs
+++ Pilha/pilha.exs
@@ -1,22 +1,27 @@
 
 defmodule Pilha do
-        import IO
-        #defstruct pilha: []
-        #def novaPilha, do: %Pilha{}
-        def push(elemento, pilha), do: pilha = [elemento | pilha]        
-        #def pop(pilha: []), do: raise("Stack is empty!")
-        def pop(pilha: [top | rest]) do
-            {top, pilha: rest}
-        end
         
-        def tamanho(pilha: pilha), do: length(pilha)
-        def exibe(pilha: pilha), do: puts pilha
+        defstruct pilha: []
+        def novaPilha, do: %Pilha{}
+        def push(%Pilha{pilha: pilha}, elemento) do 
+                %Pilha{ pilha: [elemento | pilha]}
+        end        
+        def pop(%Pilha{pilha: [topo | resto]}), do: {topo, %Pilha{pilha: resto}}
+        def pop(pilha = %Pilha{pilha: []}), do: {nil, pilha}
+        
+        def tamanho(%Pilha{pilha: pilha}), do: length(pilha)
+        
     
 end
 
-pilhaOriginal = []
-Pilha.push(2, pilhaOriginal) |> Pilha.tamanho(pilhaOriginal)
+pilhaOriginal = Pilha.novaPilha
+pilhaOriginal = pilhaOriginal |> Pilha.push(2) |> Pilha.push(3) |> Pilha.push(4)
+IO.puts "#{inspect pilhaOriginal}"
+{4, pilhaOriginal} = Pilha.pop(pilhaOriginal) #pattern matching
+IO.puts "#{inspect pilhaOriginal}"
+IO.puts "#{inspect Pilha.tamanho(pilhaOriginal)}"
 
 
-#Pilha.novaPilha |> Pilha.push(1) |> Pilha.push(2) |> Pilha.exibe()
+
+
 
